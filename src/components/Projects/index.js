c903970254de73c9fa6c23fb47b5b9472b5eaea1
Vincent Minicozzi <vinicozzimincent@gmail.com>
2023-08-17 15:01:21 -0400

    add globe transition onClick

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import './Projects.scss';
+import fableton from '../../Fableton.gif';
+import grubhubby from '../../grubhuby.gif';
+import soundspace from '../../SoundSpace.gif';
 
 const Projects = () => {
+
+    const slides = [
+        {
+          title: "SoundSpace",
+          subtitle: "MERN Stack",
+          description: "Adventure is never far away",
+          image: soundspace
+        },
+        {
+          title: "GrubHubby",
+          subtitle: "Ruby on Rails & React",
+          description: "Let your dreams come true",
+          image: grubhubby
+        },
+        {
+          title: "Fableton",
+          subtitle: "Vanilla JS",
+          description: "A piece of heaven",
+          image: fableton
+        }
+      ];
+
     return (
-        <div>Projects</div>
+        <div className="projects-container">
+            <div className="soundspace">
+                <div className="soundspace-images">
+                    <img src={soundspace} alt="SoundSpace" />
+                </div>
+                <div className="soundspace-description">
+                    <h2>SoundSpace</h2>
+                    <p>Project 1 description...</p>
+                </div>
+            </div>
+            <div className="grubhubby">
+                <div className="grubhubby-images">
+                    <img src={grubhubby} alt="GrubHubby" />
+                </div>
+                <div className="grubhubby-description">
+                    <h2>GrubHubby</h2>
+                    <p>Project 2 description...</p>
+                </div>
+            </div>
+            <div className="fableton">
+                <div className="fableton-images">
+                    <img src={fableton} alt="Fableton" />
+                </div>
+                <div className="fableton-description">
+                    <h2>Fableton</h2>
+                    <p>Project 3 description...</p>
+                </div>
+            </div>
+        </div>
     );
 };
-
 export default Projects;
--- src/components/World/index.js
+++ src/components/World/index.js
@@ -1,13 +1,18 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo} from 'react';
 import Globe from 'react-globe.gl';
 import * as THREE from 'three';
 import GlobeImage from '../../welcome.png';
 import { useNavigate } from 'react-router-dom';
+import Projects from '../Projects';
+import AboutMe from '../AboutMe';
+import Contact from '../Contact';
+import Hobbies from '../Hobbies';
 
 const World = () => {
   const globeEl = useRef();
   const [data, setData] = useState([]);
   const navigate = useNavigate();
+  const [selectedObj, setSelectedObj] = useState(null);
 
   const randomData = [
     {
@@ -86,7 +91,7 @@ const World = () => {
     const z = radiusOrbit * Math.sin(phi) * Math.sin(theta);
 
     // Create a custom three.js object (sphere) for the dot
-    const dotGeometry = new THREE.SphereGeometry(radius, 50, 50);
+    const dotGeometry = new THREE.SphereGeometry(radius, 100, 100);
     const dotMaterial = new THREE.MeshLambertMaterial({ color });
     const dotMesh = new THREE.Mesh(dotGeometry, dotMaterial);
 
@@ -101,29 +106,139 @@ const World = () => {
     Object.assign(obj.position, globeEl.current.getCoords(lat, lng, alt));
   };
 
+  // const handleCustomLayerClick = (obj, event, { lat, lng }) => {
+  //   // Find the clicked data point
+  //   navigate(obj.link);
+  //   // const clickedDataPoint = data.find((d) => d.lat === lat && d.lng === lng);
+  //   // if (clickedDataPoint) {
+  //   //   navigateToLink(clickedDataPoint.link);
+  //   // }
+  // };
+
+  // const handleCustomLayerClick = (obj, event, { lat, lng }) => {
+  //   if (obj.id === 1) {
+  //     // Move the globe to the right
+  //     globeEl.current.pointOfView({altitude: 5.0});
+  //   } else if (obj.id === 2) {
+  //     // Move the globe to the left
+  //     globeEl.current.pointOfView({altitude: 5.0});
+  //   } else if (obj.id === 3) {
+  //     // Move the globe up
+  //     globeEl.current.pointOfView({altitude: 5.0});
+  //   } else if (obj.id === 4) {
+  //     // Move the globe down
+  //     globeEl.current.pointOfView({altitude: 5.0});
+  //   }
+  // };
+
   const handleCustomLayerClick = (obj, event, { lat, lng }) => {
-    // Find the clicked data point
-    navigate(obj.link);
-    // const clickedDataPoint = data.find((d) => d.lat === lat && d.lng === lng);
-    // if (clickedDataPoint) {
-    //   navigateToLink(clickedDataPoint.link);
-    // }
+    // Define the altitude for the globe's new position
+    // const altitude = 5.0;
+    
+    // Find the DOM element with the class 'globe-container'
+    const globeContainer = document.querySelector('.globe-container');
+    
+    // Check if the globe container element exists
+    if (globeContainer) {
+      // Get the current transform value of the globe container, or set it to 'translateX(0px)' if undefined
+      const currentTransform = globeContainer.style.transform || 'translateX(0px)';
+      globeContainer.style.transition = 'transform 2s ease-in-out';
+
+      // Extract the numerical part (pixels) from the current transform value
+      const translateX = parseFloat(currentTransform.match(/-?\d+/)[0]);
+  
+      let newTranslateX = translateX;
+      
+      // Adjust the newTranslateX based on different obj.id values
+      if (obj.id === 1) {
+        newTranslateX = 450; // Move to the right
+      } else if (obj.id === 2) {
+        newTranslateX = -450; // Move to the left
+      } else if (obj.id === 3) {
+        newTranslateX = 450; // Move more to the right
+      } else if (obj.id === 4) {
+        newTranslateX = -450; // Move more to the left
+      } else {
+        newTranslateX= 0;
+      }
+    
+      // Apply the new transform value to the globe container's style, moving it horizontally
+      globeContainer.style.transform = `translateX(${newTranslateX}px)`;
+      
+      // Move the globe's point of view to the new position
+      const newAltitude = 5.0;
+      // const currentAltitude = globeEl.current.pointOfView().altitude || 0;
+      const altitudeChangeDuration = 2; // in seconds
+  
+      globeEl.current.pointOfView(
+        {altitude: newAltitude },
+        altitudeChangeDuration * 1000 // Convert seconds to milliseconds
+      );    
+    };
+          setSelectedObj(obj); 
+
   };
+
+  const renderContent = useMemo(() => {
+    if (!selectedObj) {
+      return null; // No selected object, no content to render
+    }
+
+    console.log(selectedObj)
+
+    switch (selectedObj.id) {
+      case 1:
+        return <AboutMe />;
+      case 2:
+        return <Contact />;
+      case 3:
+        return <Projects />;
+      case 4:
+        return <Hobbies />;
+      default:
+        return null; // Return null or a default component if no match
+    }
+  }, [selectedObj]);
+
+  
+  // const handleCustomLayerClick = (obj, event, { lat, lng }) => {
+  //   // Find the clicked data point
+  //   const clickedDataPoint = data.find((d) => d.lat === lat && d.lng === lng);
+  
+  //   if (clickedDataPoint) {
+  //     const { lat, lng, alt } = clickedDataPoint;
+  //     // Calculate the new camera position
+  //     const newLat = lat;
+  //     const newLng = lng + (clickedDataPoint.id === 1 ? 30 : -30); // Adjust the value based on your preference
+  //     const newAlt = alt + 0.1; // Adjust the altitude if needed
+  
+  //     // Move the globe's point of view to the new position
+  //     globeEl.current.pointOfView({ lat: newLat, lng: newLng, altitude: newAlt });
+  //   }
+  // };
 
   // const navigateToLink = (link) => {
   //   navigate(link);
   // };
 
   return (
+    <>
+    <div className="globe-container">
     <Globe
       ref={globeEl}
       globeImageUrl={GlobeImage}
+      backgroundColor="rgba(255, 0, 0, 0)"
       bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
       customLayerData={data}
       customThreeObject={customThreeObject}
       customThreeObjectUpdate={customThreeObjectUpdate}
       onCustomLayerClick={handleCustomLayerClick}
     />
+    </div>
+     <div className="info-panel" style={{ zIndex: 999 }}>
+        {renderContent}
+      </div>
+    </>
   );
 };
 
