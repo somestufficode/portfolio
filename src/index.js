550bbe375da39cebcb65f74b94bc75dbe9199480
Vincent Minicozzi <vinicozzimincent@gmail.com>
2023-08-27 22:18:04 -0400

    update browserRouter

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import World from './components/World';
-import About from './components/AboutMe'; 
-import Projects from './components/Projects';
-import Hobbies from './components/Hobbies';
-import Contact from './components/Contact';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<World />} />
-        <Route path="/aboutme" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/hobbies" element={<Hobbies />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </Router>
+    <World />
   </React.StrictMode>,
   document.getElementById('root')
 );
